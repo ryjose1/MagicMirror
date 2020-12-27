2821bb471cbbd4120f0ac3eceea35e0cfe357715
Ryan <8942922+rajose@users.noreply.github.com>
2020-12-27 01:34:53 -0500

    Personal config changes
    - Removed entries from .gitignore related to custom settings
    - Added custom css file for overriding styling
    - Removed some modules from config.js
    - Added config for MMM-CalendarExt2 module

--- config/config.js
+++ config/config.js
@@ -30,12 +30,20 @@ var config = {
 	language: "en",
 	logLevel: ["INFO", "LOG", "WARN", "ERROR"],
 	timeFormat: 24,
-	units: "metric",
+	units: "imperial",
 	// serverOnly:  true/false/"local" ,
 	// local for armv6l processors, default
 	//   starts serveronly and then starts chrome browser
 	// false, default for all NON-armv6l devices
 	// true, force serveronly mode, because you want to.. no UI on this device
+
+	// Keys for reference
+	// module	The name of the module. This can also contain the subfolder. Valid examples include clock, default/calendar and custommodules/mymodule.
+	// position	The location of the module in which the module will be loaded. Possible values are top_bar, top_left, top_center, top_right, upper_third, middle_center, lower_third, bottom_left, bottom_center, bottom_right, bottom_bar, fullscreen_above, and fullscreen_below. This field is optional but most modules require this field to set. Check the documentation of the module for more information. Multiple modules with the same position will be ordered based on the order in the configuration file.
+	// classes	A list of additional CSS classes which will be set on the module. This field is optional.
+	// header	To display a header text above the module, add the header property. This field is optional.
+	// disabled	Set disabled to true to skip creating the module. This field is optional.
+	// config	An object with the module configuration properties. Check the documentation of the module for more information. This field is optional, unless the module requires extra configuration.
 
 	modules: [
 		{
@@ -50,56 +58,33 @@ var config = {
 			position: "top_left"
 		},
 		{
-			module: "calendar",
-			header: "US Holidays",
-			position: "top_left",
+			module: "custommodules/MMM-CalendarExt2",
 			config: {
-				calendars: [
+				views: [
 					{
-						symbol: "calendar-check",
-						url: "webcal://www.calendarlabs.com/ical-calendar/ics/76/US_Holidays.ics"					}
-				]
-			}
-		},
-		{
-			module: "compliments",
-			position: "lower_third"
-		},
-		{
-			module: "currentweather",
-			position: "top_right",
-			config: {
-				location: "New York",
-				locationID: "", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-				appid: "YOUR_OPENWEATHER_API_KEY"
-			}
-		},
-		{
-			module: "weatherforecast",
-			position: "top_right",
-			header: "Weather Forecast",
-			config: {
-				location: "New York",
-				locationID: "5128581", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-				appid: "YOUR_OPENWEATHER_API_KEY"
-			}
-		},
-		{
-			module: "newsfeed",
-			position: "bottom_bar",
-			config: {
-				feeds: [
+						mode: "month",
+						position: "bottom_bar",
+						slotMaxHeight: "130px",
+						weeksFormat: "wo"
+					}
+                ],
+                scenes: [
 					{
-						title: "New York Times",
-						url: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+						name: "DEFAULT"
 					}
 				],
-				showSourceTitle: true,
-				showPublishDate: true,
-				broadcastNewsFeeds: true,
-				broadcastNewsUpdates: true
+				calendars: [
+					{
+						name: "events",
+						url: "https://calendar.google.com/calendar/ical/ryanalex.jose%40gmail.com/private-841dbfc82ffd351f99e77c337c486980/basic.ics"
+					},
+					{
+						name: "Birthdays",
+						url: "https://calendar.google.com/calendar/ical/odh22k0c18tdu4u1c0od643chc%40group.calendar.google.com/private-ec0bc1a92797386f1ff8534603109dea/basic.ics"
+					}
+				]
 			}
-		},
+		}
 	]
 };
 
